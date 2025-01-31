a71d1ad374fa058c2806807a972703cde26b3a39
Jan Kotanski <jankotan@gmail.com>
2025-01-31 10:53:23 +0100

    improve tests

--- test/NXSCreatePyEvalH5Cpp_test.py
+++ test/NXSCreatePyEvalH5Cpp_test.py
@@ -2404,6 +2404,10 @@ class NXSCreatePyEvalH5CppTest(unittest.TestCase):
         fun = sys._getframe().f_code.co_name
         print("Run: %s.%s() " % (self.__class__.__name__, fun))
 
+        if not self.fwriter.is_vds_supported():
+            print("Skip the test: VDS not supported")
+            return
+
         mfileprefix = "%s%s" % (self.__class__.__name__, fun)
         scanid = 12345
 
