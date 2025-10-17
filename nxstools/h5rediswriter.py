c8b9a5177c7c4e138e3a19aab958e67d8b29ece2
Jan Kotanski <jankotan@gmail.com>
2025-10-17 14:29:25 +0200

    Bump the version to 4.29.0 (#810)
    * fix frame per file
    
    * bump the version

--- nxstools/h5rediswriter.py
+++ nxstools/h5rediswriter.py
@@ -1617,7 +1617,7 @@ class H5RedisField(H5Field):
                     dtype=sds["dtype"],
                     shape=shape,
                     file_pattern=str(filename),
-                    frames_per_file=-1,
+                    frames_per_file=0,
                     data_path=self.path,
                     # data_path="/scan/data/%s" % dsname ,
                     info={"unit": units},
