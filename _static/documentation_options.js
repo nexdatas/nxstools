b3ef36a39e349330eccf47e94457a335fccfece5
Jan Kotanski <jankotan@gmail.com>
2026-04-10 09:02:38 +0200

    update docu

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.46.0',
+    VERSION: '4.47.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
