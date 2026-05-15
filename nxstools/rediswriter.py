46b169f6c1a2daf888937ca0b78653c55ce46067
Jan Kotanski <jankotan@gmail.com>
2026-05-15 17:45:06 +0200

    fix typo in reading acquisition_offset

--- nxstools/rediswriter.py
+++ nxstools/rediswriter.py
@@ -3117,7 +3117,7 @@ class RedisVirtualFieldLayout(filewriter.FTVirtualFieldLayout):
                     if "acquisition_offset" in plugin_def and \
                             not plugin_def["acquisition_offset"]:
                         if "server_url" in plugin_def and \
-                                not plugin_def["server_url"]:
+                                plugin_def["server_url"]:
                             acq_off = self.acquisition_on_server(
                                 plugin_def["server_url"])
                             if acq_off is not None:
