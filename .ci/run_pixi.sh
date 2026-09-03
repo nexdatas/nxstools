57af78b0627c03843de5c611ae7e5052370b77db
Jan Kotanski <jankotan@gmail.com>
2026-09-03 21:19:50 +0200

    update tests

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "install pixi"
-docker exec  ndts /bin/bash -c 'mkdir /home/tango/bin; cp test/TestServer /home/tango/bin'
+docker exec  ndts /bin/bash -c 'mkdir /home/tango/bin; cp /home/tango/test/TestServer /home/tango/bin'
+docker exec  ndts /bin/bash -c 'cp /var/lib/tango/test/TestServer /home/tango/bin'
 docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build'
 # docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build nxsconfigserver-db'
 # # /home/tango
