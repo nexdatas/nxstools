0c135439048f0a8326e572bd98bdbbe7b78c4f0a
Jan Kotanski <jankotan@gmail.com>
2026-09-07 09:10:16 +0200

    update config

--- test/NXSetUp_test.py
+++ test/NXSetUp_test.py
@@ -1,4 +1,4 @@
-\65;7006;1c#!/usr/bin/env python
+#!/usr/bin/env python
 #   This file is part of nexdatas - Tango Server for NeXus data writer
 #
 #    Copyright (C) 2012-2018 DESY, Jan Kotanski <jkotan@mail.desy.de>
